2de6d6c2d2a2de6e35c45da514a3c046e753cfc4
JHON HERBERT REIN <jhon.rein@hotmail.com>
2026-01-06 12:10:59 -0300

    Update 01_criacao_do_banco.sql

--- SQL/01_criacao_do_banco.sql
+++ SQL/01_criacao_do_banco.sql
@@ -2,9 +2,11 @@
 -- 1. CRIAÇÃO DO BANCO DE DATOS
 -- ======================================================
 -- Cria o banco apenas se ele não existir para evitar erros
+
 CREATE DATABASE IF NOT EXISTS virtual_system
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- Define que os próximos comandos serão executados neste banco
+
 USE virtual_system;
